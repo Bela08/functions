0a3430400f2a4dabaade2570602031d2dba2ad5c
marianovosolov <marianovosolov@gmail.com>
2020-03-30 09:53:02 +0200

    Updated solution for practice 3

--- solutions.R
+++ solutions.R
@@ -11,3 +11,15 @@ df<- as.data.frame(matrix(1:100,5,10))
 df
 a<- c(2,3,4,5,6)
 apply(df,2,function(x) x*a)
+
+#practice 3
+bottles99<- function(){
+  i = 99
+  while(i > 0){
+    print(paste(i,"bottles of beer on the wall",i, "bottles of beer. Take one down, pass it around -",i-1,"bottles of beer on the wall"))
+    i = i -1
+  }
+
+}
+
+bottles99()
